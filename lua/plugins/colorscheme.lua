8f0461f20c6fd2a268f94a317cc78913209d8011
Kaleb Olson <kalebroyolson+github@gmail.com>
2024-07-25 09:22:03 -0500

    new color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,25 @@
+local schemes = {
+  kanagawapaper = {
+    repo = "sho-87/kanagawa-paper.nvim",
+    name = "kanagawa-paper",
+  },
+  ares = {
+    repo = "ab-dx/ares.nvim",
+    name = "ares",
+  }
+}
+
+local scheme = schemes['ares'];
+
 return {
-  "sho-87/kanagawa-paper.nvim",
+  scheme.repo,
+  name = scheme.name,
   lazy = false,
   priority = 1000,
+  dependencies = {
+    'rktjmp/lush.nvim', -- required for ares.nvim
+  },
   config = function()
-    vim.cmd("colorscheme kanagawa-paper")
+    vim.cmd(string.format("colorscheme %s", scheme.name))
   end,
 }
