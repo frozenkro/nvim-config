845c2aba44d873852eb47123389d7021062e5945
Kaleb Olson <kalebroyolson+github@gmail.com>
2024-09-10 14:51:37 -0500

    markdownpreview + ts_ls

--- lua/modules/lspconfig.lua
+++ lua/modules/lspconfig.lua
@@ -43,6 +43,7 @@ local function lsp_keymap(bufnr)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', 'gK', vim.diagnostic.open_float)
   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
@@ -51,7 +52,7 @@ local function lsp_keymap(bufnr)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 end
 
-lspconfig.tsserver.setup{
+lspconfig.ts_ls.setup{
     on_attach = function(_, bufnr)
         lsp_keymap(bufnr)
     end,
@@ -61,77 +62,19 @@ lspconfig.gopls.setup{
         lsp_keymap(bufnr)
     end,
 }
+
+local function get_cls_sln()
+    local sln = os.getenv('CLS_SLN')
+    print('csharp_ls using: ', sln, ' as solution')
+end
 lspconfig.csharp_ls.setup{
     settings = {
         csharp = {
-            solution = os.getenv('CLS_SLN'),
+            solution = get_cls_sln(),
         }
     },
     on_attach = function(_, bufnr)
+        print('attaching with CLS_SLN set to ', os.getenv('CLS_SLN'))
         lsp_keymap(bufnr)
     end,
 }
--- lspconfig.omnisharp.setup({
---     cmd = { "dotnet", os.getenv("OMNISHARP_DLL") },
---     vim.cmd([[
---         let g:OmniSharp_autoselect_existing_sln = 1 
---     ]]),
---     root_dir = lspconfig.util.root_pattern('*.sln', '*.csproj'),
---     on_attach = function(client, bufnr)
---         print('omnisharp attached')
---         -- replaces vim.lsp.buf.definition()
---         vim.keymap.set('n', '<leader>gd', require'omnisharp_extended'.lsp_definition,
---             { desc = "go to definition" })
-
---         -- replaces vim.lsp.buf.type_definition()
---         vim.keymap.set('n', '<leader>D', require'omnisharp_extended'.lsp_type_definition,
---             { desc = "go to type definition" })
-
---         -- replaces vim.lsp.buf.references()
---         vim.keymap.set('n', '<leader>gr', require'omnisharp_extended'.lsp_references,
---             { desc = "find refs" })
-
---         -- replaces vim.lsp.buf.implementation()
---         vim.keymap.set('n', '<leader>gi', require'omnisharp_extended'.lsp_implementation,
---             { desc = "go to implementation" })
---     end,
-
---     settings = {
---       FormattingOptions = {
---         -- Enables support for reading code style, naming convention and analyzer
---         -- settings from .editorconfig.
---         EnableEditorConfigSupport = true,
---         -- Specifies whether 'using' directives should be grouped and sorted during
---         -- document formatting.
---         OrganizeImports = nil,
---       },
---       MsBuild = {
---         -- If true, MSBuild project system will only load projects for files that
---         -- were opened in the editor. This setting is useful for big C# codebases
---         -- and allows for faster initialization of code navigation features only
---         -- for projects that are relevant to code that is being edited. With this
---         -- setting enabled OmniSharp may load fewer projects and may thus display
---         -- incomplete reference lists for symbols.
---         LoadProjectsOnDemand = nil,
---       },
---       RoslynExtensionsOptions = {
---         -- Enables support for roslyn analyzers, code fixes and rulesets.
---         EnableAnalyzersSupport = nil,
---         -- Enables support for showing unimported types and unimported extension
---         -- methods in completion lists. When committed, the appropriate using
---         -- directive will be added at the top of the current file. This option can
---         -- have a negative impact on initial completion responsiveness,
---         -- particularly for the first few completion sessions after opening a
---         -- solution.
---         EnableImportCompletion = nil,
---         -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
---         -- true
---         AnalyzeOpenDocumentsOnly = nil,
---       },
---       Sdk = {
---         -- Specifies whether to include preview versions of the .NET SDK when
---         -- determining which version to use for project loading.
---         IncludePrereleases = true,
---       },
---     },
--- })
