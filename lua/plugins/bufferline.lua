9ed21fa098c79e11891d31e45cb5426186102d99
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-02-04 09:31:41 -0600

    buffer nums in bufferine

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,10 +1,14 @@
--- Visualizes open buffers as tabs 
+-- Visualizes open buffers as tabs
 return {
-  'akinsho/bufferline.nvim',
-  dependencies = { 'kyazdani42/nvim-web-devicons' },
-  opts = {
-    options = {
-      themable = true,
+    'akinsho/bufferline.nvim',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    opts = {
+        options = {
+            themable = true,
+            show_close_icon = false,
+            numbers = function(opts)
+                return opts.ordinal
+            end,
+        },
     },
-  },
 }
