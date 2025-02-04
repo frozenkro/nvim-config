c8e359a6e4aa653aceef2635d48db40a06e2ed84
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-02-04 09:33:22 -0600

    remap buffer nav

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,7 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>',
-  { desc = 'Toggle Neotree file explorer' })
+    { desc = 'Toggle Neotree file explorer' })
 
 vim.keymap.set('n', '<C-,>', '<Cmd>vertical resize -10<CR>')
 vim.keymap.set('n', '<C-.>', '<Cmd>vertical resize +10<CR>')
@@ -16,11 +16,11 @@ vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]])
 vim.keymap.set("n", "+", [[<cmd>horizontal resize +10<cr>]])
 vim.keymap.set("n", "_", [[<cmd>horizontal resize -10<cr>]])
 
-vim.keymap.set('n', '[b', '<Cmd>bprev<CR>')
-vim.keymap.set('n', ']b', '<Cmd>bnext<CR>')
+vim.keymap.set('n', '[[', '<Cmd>bprev<CR>')
+vim.keymap.set('n', ']]', '<Cmd>bnext<CR>')
 vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>",
-  { noremap = true, silent = true })
+    { noremap = true, silent = true })
 vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
@@ -29,8 +29,8 @@ vim.cmd([[cab cc CodeCompanion]])
 vim.keymap.set('n', '<leader>d', function() notify.dismiss({ pending = true, silent = true }) end)
 
 local close_buffer_keep_window_open = function()
-  local bufnum = vim.fn.bufnr()
-  vim.cmd('bprev')
-  vim.cmd(string.format('bd%s', bufnum))
+    local bufnum = vim.fn.bufnr()
+    vim.cmd('bprev')
+    vim.cmd(string.format('bd%s', bufnum))
 end
 vim.keymap.set('n', '<leader>q', close_buffer_keep_window_open)
