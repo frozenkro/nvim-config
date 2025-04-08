79adfb17f1626163e9fe06bba4cb9210b80389ff
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-04-08 11:57:44 -0500

    local copilot config

---
+++ lua/modules/copilot.lua
@@ -0,0 +1,4 @@
+-- This is already the default but its not in the docs
+-- So I am just leaving it here for reference
+
+-- vim.g.copilot_settings = { selectedCompletionModel = 'gpt-4o-copilot' }
--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,54 +1,68 @@
+local want = require('utils.want')
+local local_cc, local_cc_err = want('local.codecompanion')
+
 local ollama_host = os.getenv("OLLAMA_HOST") -- include :11434 in route
-return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "echasnovski/mini.pick",
-  },
-  opts = {
-    adapters = {
-      ollama_hosted = function()
+local adapters = {
+    ollama_hosted = function()
         return require("codecompanion.adapters").extend("ollama", {
-          name = "ollama_hosted",
-          env = {
-            url = ollama_host
-          },
-          schema = {
-            model = {
-              default = "qwen2.5-coder:32b",
+            name = "ollama_hosted",
+            env = {
+                url = ollama_host
             },
-          },
+            schema = {
+                model = {
+                    default = "qwen2.5-coder:32b",
+                },
+            },
         })
-      end,
-    },
-    display = {
-      action_palette = {
-        height = 6,
-        width = 10,
-        provider = "default",
-      },
-      chat = {
-        show_settings = "true",
-        window = {
-          position = "right", -- "right" interacts strangely with neo-tree
-          width = 0.2,
-          opts = {
-            list = true,
-          },
-        },
-      },
-    },
-    strategies = {
-      chat = {
+    end,
+}
+local strategies = {
+    chat = {
         adapter = "ollama_hosted",
-      },
-      inline = {
+    },
+    inline = {
         adapter = "ollama_hosted",
-      },
+    },
+}
+
+if (local_cc) then
+    adapters = local_cc.adapters
+    strategies = local_cc.strategies
+end
+
+
+
+
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "echasnovski/mini.pick",
     },
     opts = {
-      log_level = "DEBUG",
+        adapters = adapters,
+        display = {
+            action_palette = {
+                height = 6,
+                width = 10,
+                provider = "default",
+            },
+            chat = {
+                show_settings = "true",
+                window = {
+                    position = "right", -- "right" interacts strangely with neo-tree
+                    width = 0.2,
+                    opts = {
+                        list = true,
+                    },
+                },
+            },
+        },
+        strategies = strategies,
+        opts = {
+            log_level = "DEBUG",
+        },
     },
-  },
 }
