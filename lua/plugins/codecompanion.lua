16acb23d277c0d50c53cc0509fabfade35ddb0e4
kaup <kalebroyolson+github@gmail.com>
2025-06-18 23:56:04 -0500

    fix mcp extension

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -64,15 +64,15 @@ return {
     opts = {
       log_level = "DEBUG",
     },
-  },
-  extensions = {
-    mcphub = {
-      callback = "mcphub.extensions.codecompanion",
-      opts = {
-        show_result_in_chat = true, -- Show mcp tool results in chat
-        make_vars = true,           -- Convert resources to #variables
-        make_slash_commands = true, -- Add prompts as /slash commands
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          show_result_in_chat = true, -- Show mcp tool results in chat
+          make_vars = true,           -- Convert resources to #variables
+          make_slash_commands = true, -- Add prompts as /slash commands
+        }
       }
-    }
+    },
   },
 }
