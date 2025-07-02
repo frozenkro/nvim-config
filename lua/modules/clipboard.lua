132cdcce4490d03823a267c1264ab110dc9ee8e8
Kaleb Olson <kaleb.olson@avionte.com>
2025-07-02 13:44:38 -0500

    Fix clipboard on wsl

--- lua/modules/clipboard.lua
+++ lua/modules/clipboard.lua
@@ -1,14 +1,14 @@
 if vim.fn.has('win32') ~= 1 and vim.fn.has('wsl') ~= 1 then
-    vim.g.clipboard = {
-        name = 'WL-Clipboard',
-        copy = {
-            ['+'] = 'wl-copy',
-            ['*'] = 'wl-copy',
-        },
-        paste = {
-            ['+'] = 'wl-paste',
-            ['*'] = 'wl-paste',
-        },
-        cache_enabled = 1,
-    }
+  vim.g.clipboard = {
+    name = 'WL-Clipboard',
+    copy = {
+      ['+'] = 'wl-copy',
+      ['*'] = 'wl-copy',
+    },
+    paste = {
+      ['+'] = 'wl-paste',
+      ['*'] = 'wl-paste',
+    },
+    cache_enabled = 1,
+  }
 end
