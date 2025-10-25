f1fa353b6c5140605acfca99cd9e9455c54707dd
kaup <kalebroyolson+github@gmail.com>
2025-10-24 21:34:26 -0500

    get oily

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 local notify = require('notify')
+local oil = require('oil')
 
 local ff_no_ignore = function() builtin.find_files({ no_ignore = true }) end
 
@@ -9,8 +10,8 @@ vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>',
-    { desc = 'Toggle Neotree file explorer' })
+vim.keymap.set('n', '<leader>e', oil.toggle_float,
+  { desc = 'Toggle oil browser' })
 
 vim.keymap.set('n', '<C-,>', '<Cmd>vertical resize -10<CR>')
 vim.keymap.set('n', '<C-.>', '<Cmd>vertical resize +10<CR>')
@@ -25,7 +26,7 @@ vim.keymap.set('n', '<S-Tab>', '<Cmd>bprev<CR>')
 vim.keymap.set('n', '<Tab>', '<Cmd>bnext<CR>')
 vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>",
-    { noremap = true, silent = true })
+  { noremap = true, silent = true })
 vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
@@ -34,14 +35,14 @@ vim.cmd([[cab cc CodeCompanion]])
 vim.keymap.set('n', '<leader>d', function() notify.dismiss({ pending = true, silent = true }) end)
 
 local close_buffer_keep_window_open = function(force)
-    local bufnum = vim.fn.bufnr()
-    vim.cmd('bprev')
+  local bufnum = vim.fn.bufnr()
+  vim.cmd('bprev')
 
-    forceStr = ''
-    if force then
-        forceStr = '!'
-    end
-    vim.cmd(string.format('bd%s%s', forceStr, bufnum))
+  forceStr = ''
+  if force then
+    forceStr = '!'
+  end
+  vim.cmd(string.format('bd%s%s', forceStr, bufnum))
 end
 
 local close_buffer_keep_window_open_soft = function() close_buffer_keep_window_open(false) end
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,40 +1,41 @@
-return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = "v3.x",
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
-  },
-  opts = {
-    close_if_last_window = true,
-    popup_border_style = "rounded",
-    enable_git_status = true,
-    window = {
-      position = "float",
-    },
-    filesystem = {
-      bind_to_cwd = true,
-      cwd_target = {
-        current = "window",
-      },
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-      window = {
-        mappings = {
-          ["F"] = "fuzzy_finder",
-          ["/"] = "noop",
-          ["z"] = "noop",
-          ["Z"] = "close_all_nodes",
-        }
-      }
-    }
-  },
-  config = function(_, opts)
-    require('neo-tree').setup(opts)
-    --vim.cmd('Neotree')
-  end,
-}
+return {}
+--return {
+--  'nvim-neo-tree/neo-tree.nvim',
+--  branch = "v3.x",
+--  dependencies = {
+--    'nvim-lua/plenary.nvim',
+--    'nvim-tree/nvim-web-devicons',
+--    'MunifTanjim/nui.nvim',
+--  },
+--  opts = {
+--    close_if_last_window = true,
+--    popup_border_style = "rounded",
+--    enable_git_status = true,
+--    window = {
+--      position = "float",
+--    },
+--    filesystem = {
+--      bind_to_cwd = true,
+--      cwd_target = {
+--        current = "window",
+--      },
+--      filtered_items = {
+--        visible = true,
+--        hide_dotfiles = false,
+--        hide_gitignored = true,
+--      },
+--      window = {
+--        mappings = {
+--          ["F"] = "fuzzy_finder",
+--          ["/"] = "noop",
+--          ["z"] = "noop",
+--          ["Z"] = "close_all_nodes",
+--        }
+--      }
+--    }
+--  },
+--  config = function(_, opts)
+--    require('neo-tree').setup(opts)
+--    --vim.cmd('Neotree')
+--  end,
+--}
