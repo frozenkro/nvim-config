5750087095d3942a6017db6b1de8b0c00f9a001a
Kaleb Olson <kalebroyolson+github@gmail.com>
2026-01-16 10:07:59 -0600

    Folding bindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,7 +13,7 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fr', builtin.resume, {})
 
 vim.keymap.set('n', '<leader>e', oil.toggle_float,
-  { desc = 'Toggle oil browser' })
+    { desc = 'Toggle oil browser' })
 
 vim.keymap.set('n', '<C-,>', '<Cmd>vertical resize -10<CR>')
 vim.keymap.set('n', '<C-.>', '<Cmd>vertical resize +10<CR>')
@@ -28,7 +28,7 @@ vim.keymap.set('n', '<S-Tab>', '<Cmd>bprev<CR>')
 vim.keymap.set('n', '<Tab>', '<Cmd>bnext<CR>')
 vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>",
-  { noremap = true, silent = true })
+    { noremap = true, silent = true })
 vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
@@ -37,14 +37,14 @@ vim.cmd([[cab cc CodeCompanion]])
 vim.keymap.set('n', '<leader>d', function() notify.dismiss({ pending = true, silent = true }) end)
 
 local close_buffer_keep_window_open = function(force)
-  local bufnum = vim.fn.bufnr()
-  vim.cmd('bprev')
+    local bufnum = vim.fn.bufnr()
+    vim.cmd('bprev')
 
-  forceStr = ''
-  if force then
-    forceStr = '!'
-  end
-  vim.cmd(string.format('bd%s%s', forceStr, bufnum))
+    forceStr = ''
+    if force then
+        forceStr = '!'
+    end
+    vim.cmd(string.format('bd%s%s', forceStr, bufnum))
 end
 
 local close_buffer_keep_window_open_soft = function() close_buffer_keep_window_open(false) end
@@ -63,7 +63,8 @@ vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
 vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
-vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>lp',
+    function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
 vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
 vim.keymap.set('n', '<Leader>dl', function() require('dap').list_breakpoints() end)
 vim.keymap.set('n', '<Leader>dc', function() require('dap').clear_breakpoints() end)
@@ -71,19 +72,28 @@ vim.keymap.set('n', '<Leader>dp', function() require('dap').pause() end)
 vim.keymap.set('n', '<Leader>df', function() require('dap').focus_frame() end)
 vim.keymap.set('n', '<Leader>du', function() require('dap').up() end)
 vim.keymap.set('n', '<Leader>dd', function() require('dap').down() end)
-vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
-  require('dap.ui.widgets').hover()
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+    require('dap.ui.widgets').hover()
 end)
-vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
-  require('dap.ui.widgets').preview()
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+    require('dap.ui.widgets').preview()
 end)
 vim.keymap.set('n', '<Leader>df', function()
-  local widgets = require('dap.ui.widgets')
-  widgets.centered_float(widgets.frames)
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.frames)
 end)
 vim.keymap.set('n', '<Leader>ds', function()
-  local widgets = require('dap.ui.widgets')
-  widgets.centered_float(widgets.scopes)
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.scopes)
 end)
 
 vim.keymap.set('n', '<Leader>dv', '<Cmd>DapViewOpen<CR>')
+
+local function collapse_all()
+    vim.cmd('syntax on')
+    vim.cmd('set foldmethod=syntax')
+    vim.cmd('set foldlevel=0')
+end
+vim.keymap.set('n', '<Leader>c', collapse_all, { desc = 'recursive fold entire file based on filetype' })
+vim.keymap.set('n', 'left', '<Cmd>foldclose<CR>', { desc = 'close fold at cursor' })
+vim.keymap.set('n', 'right', '<Cmd>foldopen<CR>', { desc = 'open fold at cursor' })
