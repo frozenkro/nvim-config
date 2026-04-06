2153b085e2ec1a82e0d6e0f1981ed6e6f741214f
Kaleb Olson <kalebroyolson+github@gmail.com>
2026-04-06 16:39:13 -0500

    Fix auto update buffer on tmux

---
+++ lua/autocmds.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+    pattern = "*",
+    callback = function()
+        if vim.fn.mode() ~= "c" then
+            vim.cmd("checktime")
+        end
+    end
+})
